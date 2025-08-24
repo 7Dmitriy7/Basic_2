335ad4da2dcf7d3c4554dba9c1556f6849f6ed14
Kozhemyakin Dmitriy <keftemeeee.2003@gmail.com>
2025-08-24 21:54:53 +0300

    Добавил сервисную панель

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log('Happy developing ✨')
+document.addEventListener('DOMContentLoaded', initSwiper);
+window.addEventListener('resize', initSwiper);
+
+let swiperElement = null;
+
+function initSwiper() {
+  const swiperEl = document.querySelector('.service__swiper');
+
+  if (!swiperEl) return;
+
+  if (window.innerWidth < 768 && !swiperElement) {
+    swiperElement = new Swiper(swiperEl, {
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      centeredSlidesBounds: true,
+      spaceBetween: 16,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  } else if (window.innerWidth >= 768 && swiperElement) {
+    swiperElement.destroy(true, true);
+    swiperElement = null;
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const swiperContainer = document.querySelector(".service__swiper");
+  const furtherContainer = document.querySelector(".service__further");
+  const furtherText = document.querySelector(".service__further-text");
+
+
+  furtherContainer.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    swiperContainer.classList.toggle("show-all");
+
+    if (swiperContainer.classList.contains ("show-all") ) {
+      furtherText.textContent = "Скрыть";
+    } else {
+      furtherText.textContent = "Показать все";
+    }
+  });
+});
